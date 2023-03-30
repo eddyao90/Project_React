f7e584601d9b3f8bf1738c27f6c6d8be7b18a052
Edson <eddyao1990@gmail.com>
2023-03-30 03:15:58 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 import AuthContext from './contexts/AuthContext'
 import Login from './views/Login/Login'
 import Profile from './views/Profile/Profile'
 
 
 function App() {
-  const { isAuthLoaded } = useContext(AuthContext);
   return (
     <div className="App">
     <Navbar />
       <div className='container my-3'>
-        {!isAuthLoaded
-        ? <p>Loading...</p>
-        : (
-        <Routes>
-          <Route path="Login" element={<Login />} />
-
+      <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } />
         </Routes>
-        )}
       </div>
     </div>
   )
-  }
+}
 
 export default App
