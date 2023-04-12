eeb53cc7df902a6d3cf306b3c8f25fe3edbc56a8
Edson <eddyao1990@gmail.com>
2023-04-12 11:59:12 +0200

    post working

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,9 +1,4 @@
 import { createHttp } from './BaseService';
-import axios from 'axios';
 
 const authenticatedHttp = createHttp(true);
 const unauthenticatedHttp = createHttp(false);
-
-export const fetchPosts = () => axios.get('/posts');
-export const createPost = (newPost) => axios.post('/posts', newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${http}/${id}`, updatedPost);
